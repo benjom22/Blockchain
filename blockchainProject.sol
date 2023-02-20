cc2b802ca89b394498f60bf493c79c12c935fc20
benjom22 <bmehanovic12@gmail.com>
2023-02-20 13:31:36 +0100

    Final Project Version

--- blockchainProject.sol
+++ blockchainProject.sol
@@ -1,39 +1,52 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Charity {
     address payable public beneficiary;
-    mapping(address => uint) public donations;
+    mapping(uint=>Donator) public donators;
     address public owner;
-    event Donated(address indexed _from, uint _value);
+    uint256 counter=0;
+
 
     constructor() {
         owner = msg.sender;
     }
 
-    function donate() public payable {
-        require(msg.value > 0, "Donation must be greater than 0 ETH");
-        donations[msg.sender] += msg.value;
-        emit Donated(msg.sender, msg.value);
+    struct Donator{
+        uint _id;
+        address sender;
+        uint amount;
     }
 
-   /* function viewDonations() public view returns (address[] memory, uint[] memory) {
-        address[] memory donors = new address[](address(0));
-        uint[] memory amounts = new uint[](0);
-        for (uint i=0; i<donations.length; i++){
-            donors.push(user);
-            amounts.push(donations[user]);
-        }
-        return (donors, amounts);
-    }*/
+     function GetMessage() public pure returns (string memory) {
+        return "Thank You for Your donation to our charity";
+    }
+
+    modifier donationVal(){
+           require(msg.value > 0, "Donation must be greater than 0 ETH");
+           _;
+    }
+
+    function donate() public payable donationVal {
+        incrementCount();
+        donators[counter]=Donator(counter, msg.sender, msg.value);
+    }
+
+    function incrementCount() internal{
+        counter+=1;
+    }
 
     function viewBalance() public view returns (uint) {
         return address(this).balance;
     }
 
-    function withdraw() public {
+    modifier onlyOwner(){
         require(msg.sender == owner, "Only owner can withdraw");
+        _;
+    }
+
+    function withdraw() public onlyOwner {
         beneficiary.transfer(address(this).balance);
     }
+
 }
